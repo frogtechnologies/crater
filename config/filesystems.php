806ba6c5588b7cf6e1a8852895c4c825369fdf02
Miller Adulu <milleradulu@gmail.com>
2022-01-28 23:58:20 +0300

    Add FTP root

--- config/filesystems.php
+++ config/filesystems.php
@@ -104,7 +104,7 @@ return [
 
             // Optional FTP Settings...
             'port' => env('FTP_PORT', 21),
-            // 'root' => env('FTP_ROOT'),
+            'root' => env('FTP_ROOT'),
             // 'passive' => true,
             // 'ssl' => true,
             // 'timeout' => 30,
